a9b3c34fee466085c8bace0aed6f0982bc7b1799
Blessly Pera <pera.blessly@gmail.com>
2025-06-01 19:51:34 +0800

    save

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,21 @@
 import express from 'express';
+import cors from 'cors';
 import sql from './sql.ts';
 
 const app = express();
 const port = 8080;
 
+app.use(cors({ origin: true }));
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/tasks', async (req, res) => {
+  const tasks = await sql`SELECT * FROM tasks`;
+  res.json(tasks);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-const asd = await sql`SELECT * FROM tasks`;
-console.log(asd);
